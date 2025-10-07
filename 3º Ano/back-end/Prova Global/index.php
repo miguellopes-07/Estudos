ade993096fbb51c369b672a8c67decb502af75ff
Miguel Lopes De Oliveira Silva <miguelloliveirasilva@gmail.com>
2025-10-07 15:26:26 -0300

    Correção
    Finalizamos a prova

--- 3º Ano/back-end/Prova Global/index.php
+++ 3º Ano/back-end/Prova Global/index.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PC-Pronto</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="carrinho.css">
 </head>
 
 <body>
     <header>
         <h1>PC pronto</h1>
-        <ul>
-            <li><a href="">Loja</a></li>
-            <li><a href="">Carrinho</a></li>
-            <li><a href="">Sobre</a></li>
+        <ul class="lista">
+            <li class="linkzin"><a href="">Loja</a></li>
+            <li class="linkzin"><a class="carrinho" href="">Carrinho</a></li>
+            <li class="linkzin"><a href="">Sobre</a></li>
         </ul>
     </header>
 
@@ -36,29 +36,34 @@
     <main>
         <h1>Carrinho &#x1F6D2</h1>
         <p>Vamos calcular o valor do produto</p>
-        <p>Informe os dados necessarios</p>
-        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
-            <fieldset>
-                <legend>Produto</legend>
-                <label for="preco">Qual o valor do produto desejado?</label>
-                <input type="number" name="preco" id="preco" min="0.01" step="0.01" required value="<?= $produto ?>">
-                <br>
-                <br>
-                <label for="quantia">Quantos desse produto você deseja?</label>
-                <input type="number" name="quantia" id="quantia" min="1" value="<?= $quantia ?>">
-
-                <br>
-                <br>
-            </fieldset>
-            <fieldset>
-                <legend>Entrega</legend>
-                <label for="cep">CEP:</label>
-                <input type="number" name="cep" id="cep" minlength="8" maxlength="8" required placeholder="35188000" value="<?= $cep ?>">
-
-            </fieldset>
-            <input type="submit" value="Calcular">
-
-        </form>
+        <p>Informe os dados necessarios:</p>
+        <div>
+            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+                <fieldset>
+                    <legend>Produto</legend>
+                    <label for="preco">Qual o valor do produto desejado?</label>
+                    <br>
+                    <br>
+                    <input type="number" name="preco" id="preco" min="0.01" step="0.01" required value="<?= $produto ?>">
+                    <br>
+                    <br>
+                    <label for="quantia">Quantos desse produto você deseja?</label>
+                    <br>
+                    <br>
+                    <input type="number" name="quantia" id="quantia" min="1" value="<?= $quantia ?>">
+                    <br>
+            
+                </fieldset>
+                <fieldset>
+                    <legend>Entrega</legend>
+                    <label for="cep">CEP</label>
+                    <br>
+                    <br>
+                    <input type="number" name="cep" id="cep" minlength="8" maxlength="8" required placeholder="35188000" value="<?= $cep ?>">
+                </fieldset>
+                <input class="calcular" type="submit" value="Calcular">
+            </form>
+        </div>
 
     </main>
 
@@ -101,7 +106,7 @@
             <li>Subtotal: <strong>R$<?= number_format($subtotal, 2, ",", ".") ?></strong></li>
             <li>Imposto (12,5%): R$<?= number_format($imposto, 2, ",", ".") ?></li>
             <li>Total Final: R$<?= number_format($total_final, 2, ",", ".") ?></li>
-            <li>Frete: R$<?= number_format($frete, ",", ".") ?></li>
+            <li>Frete: R$<?= number_format($frete, 2, ",", ".") ?></li>
             <li>Valor Total com Frete: R$<?= number_format($valor_com_frete, 2, ",", ".") ?></li>
             <li>Cotação do Dólar Hoje: R$<?= number_format($cotacao, 2, ",", ".") ?></li>
             <li>Total em Dólares: US$<?= number_format($preco_dolar, 2, ".", ",") ?></li>
@@ -109,12 +114,13 @@
         </p>
         <h2>Entrega</h2>
         <ul>
-            <li><?= $mes ?> Meses</li>
-            <li><?= $semana ?> Semanas</li>
-            <li><?= $dia ?> Dias</li>
+            <li><?= $mes?> Meses</li>
+            <li><?= $semana?> Semanas</li>
+            <li><?= $dia?> Dias</li>
         </ul>
 
     </section>
+    <hr>
 
     <footer>
         <p><small>Miguel - PC Pronto | &copy; 2025</small></p>
